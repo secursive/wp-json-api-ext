84d8b9d85e0cf41a1cd72b587cfcf372e23c2097
secursive <akbar@secursive.com>
2013-02-26 22:36:49 +0500

    Adding new controller to JSON Api plugin

---
+++ secursive.php
@@ -0,0 +1,11 @@
+﻿<?php
+/*
+Controller name: JSON-API Extensions (Secursive)
+Controller description: Extension controller for JSON API plugin from Secursive - https://github.com/secursive/wp-json-api-ext
+*/
+
+class JSON_API_Secursive_Controller {
+    
+}
+
+?>
