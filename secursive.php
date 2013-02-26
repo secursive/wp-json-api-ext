41ea2bdb770dc7c13880d585d1ff9008236d118d
secursive <akbar@secursive.com>
2013-02-26 22:44:38 +0500

    Implementing get_categories_posts function (Tests successful)

--- secursive.php
+++ secursive.php
@@ -6,6 +6,40 @@ Controller description: Extension controller for JSON API plugin from Secursive 
 
 class JSON_API_Secursive_Controller {
 
+    // Function for retrieving posts from multiple categories (comma separated input)
+    public function get_categories_posts() {
+    global $json_api, $post;
+    extract($json_api->query->get(array('id', 'slug')));
+    // Retrieving posts for (comma-separated) category id(s) or slug(s)
+    if ($id) {
+      $posts = $json_api->introspector->get_posts(array(
+        'cat' => $id
+      ));
+    } else if ($slug) {
+      $posts = $json_api->introspector->get_posts(array(
+        'category_name' => $slug
+      ));
+    } else {
+      $json_api->error("Include 'id' or 'slug' var in your request. Separate multiple categories with comma.");
+    }
+    // Retrieving meta data (including MagicFields) for each post
+	foreach ($posts as $post) {
+	  $meta = get_post_meta($post->id);
+	  if (empty($meta)) {
+	    $meta = array();
+	  }
+    // Embedding meta data into output post objects
+	  foreach ($meta as $meta_key => $meta_val) {
+        // Excluding meta keys starting with underscore (internal keys)
+	    if (substr($meta_key,0,1) != "_") {
+	      $post->$meta_key = $meta_val;
+		}
+	  }
+    }
+    // Returning result posts
+    return $this->posts_result($posts);
+  }
+
   protected function posts_result($posts) {
     global $wp_query;
     return array(
