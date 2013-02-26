ceffa0e87c151806f28b79461266380abe4fad31
secursive <akbar@secursive.com>
2013-02-26 22:38:33 +0500

    Grabbing some useful functions for result object encoding from existing core.php controller

--- secursive.php
+++ secursive.php
@@ -5,7 +5,29 @@ Controller description: Extension controller for JSON API plugin from Secursive 
 */
 
 class JSON_API_Secursive_Controller {
-    
+
+  protected function posts_result($posts) {
+    global $wp_query;
+    return array(
+      'count' => count($posts),
+      'count_total' => (int) $wp_query->found_posts,
+      'pages' => $wp_query->max_num_pages,
+      'posts' => $posts
+    );
+  }
+  
+  protected function posts_object_result($posts, $object) {
+    global $wp_query;
+    // Convert something like "JSON_API_Category" into "category"
+    $object_key = strtolower(substr(get_class($object), 9));
+    return array(
+      'count' => count($posts),
+      'pages' => (int) $wp_query->max_num_pages,
+      $object_key => $object,
+      'posts' => $posts
+    );
+  }
+  
 }
 
 ?>
